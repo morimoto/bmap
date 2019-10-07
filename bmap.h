8f6d84d831859964ffb97342c9e404718d8aaf50
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-10-07 13:52:38 +0900

    bmap.h: use Linux coding style
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- bmap.h
+++ bmap.h
@@ -1,7 +1,7 @@
 /************************************************************************
 
 
-                                bmap.h
+			bmap.h
 
 Copyright (c) Kuninori Morimoto <morimoto.kuninori@renesas.com>
 
@@ -25,66 +25,63 @@ Copyright (c) Kuninori Morimoto <morimoto.kuninori@renesas.com>
 #define u16 unsigned short
 #define u8  unsigned char
 
-
 //=====================================
 //
-//          version
+//	version
 //
 //=====================================
 #define VERSION "0.0.2"  // see VERSION file for detail
 
 //=====================================
 //
-//          BitMap
+//	BitMap
 //
 //=====================================
 #pragma pack(2)
-struct BitMapInfo
-{
-    u16 type;
-    u32 size;
-    u32 reserved;
-    u32 offset;
+struct BitMapInfo {
+	u16 type;
+	u32 size;
+	u32 reserved;
+	u32 offset;
 };
 
-struct BitMapHeader
-{
-    u32  size;
-    u32  width;
-    u32  height;
-    u16  planes;
-    u16  bit_count;
-    u32  compression;
-    u32  image_size;
-    long x_pels_per_meter;
-    long y_pels_per_meter;
-    u32  colors_used;
-    u32  colors_imported;
+struct BitMapHeader {
+	u32  size;
+	u32  width;
+	u32  height;
+	u16  planes;
+	u16  bit_count;
+	u32  compression;
+	u32  image_size;
+	long x_pels_per_meter;
+	long y_pels_per_meter;
+	u32  colors_used;
+	u32  colors_imported;
 };
 
 struct BitMap {
-    int id;
-    size_t size;
-    struct BitMapInfo   *pinfo;
-    struct BitMapHeader *phead;
-    void (*getcolor)( struct BitMap *pBit , u8 *pRed , u8 *pGreen , u8 *pBlue );
-    uintptr_t map;
-    u32 seek;
+	int id;
+	size_t size;
+	struct BitMapInfo   *pinfo;
+	struct BitMapHeader *phead;
+	void (*getcolor)(struct BitMap *pBit, u8 *pRed, u8 *pGreen, u8 *pBlue);
+	uintptr_t map;
+	u32 seek;
 };
 
-void CloseBitMap( struct BitMap *pBit );
-struct BitMap* OpenBitMap( const char *pFile );
-const u8* GetBitMapLine( int y , struct BitMap *pBit );
-bool BitMapSeek( u32 nY , struct BitMap *pBit );
+void CloseBitMap(struct BitMap *pBit);
+struct BitMap *OpenBitMap(const char *pFile);
+const u8 *GetBitMapLine(int y, struct BitMap *pBit);
+bool BitMapSeek(u32 nY, struct BitMap *pBit);
 
 //=====================================
 //
-//          Frame Buffer
+//	Frame Buffer
 //
 //=====================================
 
-void CloseFrameBuffer( void );
-bool OpenFrameBuffer( const char *pFrame );
-void DrawBitMap(struct BitMap *pBit , u32 startx, u32 starty );
+void CloseFrameBuffer(void);
+bool OpenFrameBuffer(const char *pFrame);
+void DrawBitMap(struct BitMap *pBit, u32 startx, u32 starty);
 
 #endif /* _BMAP_H_ */
